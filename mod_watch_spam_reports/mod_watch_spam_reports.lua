8752587c03c7b6dbe2819f40d99c2f8873e0424e
Martin Dosch <martin@mdosch.de>
2020-06-24 09:01:55 +0200

    mod_watch_spam_reports: Create report message outside of the admin notification loop

--- mod_watch_spam_reports/mod_watch_spam_reports.lua
+++ mod_watch_spam_reports/mod_watch_spam_reports.lua
@@ -7,10 +7,11 @@ module:depends("spam_reporting")
 
 module:hook("spam_reporting/spam-report", function(event)
 	local reporter_bare_jid = jid.bare(event.stanza.attr.from)
+       local report = reporter_bare_jid.." reported "..event.jid.." as spammer: "..event.reason
 	for admin_jid in admins
 		do
 			module:send(st.message({from=host,
 			type="chat",to=admin_jid},
-			reporter_bare_jid.." reported "..event.jid.." as spammer: "..event.reason));
+                       report));
 		end
 end)
