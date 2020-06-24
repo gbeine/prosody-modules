8bb49c1528a3e767800ded8537268b7823ca4470
Martin Dosch <martin@mdosch.de>
2020-06-24 08:51:07 +0200

    mod_watch_spam_reports: Show reporters bare JID instead of full JID

--- mod_watch_spam_reports/mod_watch_spam_reports.lua
+++ mod_watch_spam_reports/mod_watch_spam_reports.lua
@@ -1,3 +1,4 @@
+local jid = require "util.jid";
 local st = require "util.stanza";
 local admins = module:get_option_inherited_set("admins");
 local host = module.host;
@@ -5,10 +6,11 @@ local host = module.host;
 module:depends("spam_reporting")
 
 module:hook("spam_reporting/spam-report", function(event)
+	local reporter_bare_jid = jid.bare(event.stanza.attr.from)
 	for admin_jid in admins
 		do
 			module:send(st.message({from=host,
 			type="chat",to=admin_jid},
-			event.stanza.attr.from.." reported "..event.jid.." as spammer: "..event.reason));
+			reporter_bare_jid.." reported "..event.jid.." as spammer: "..event.reason));
 		end
 end)
