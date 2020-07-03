7f32abae51c01ce0c9c66eb4bfb02989a7a447d6
Martin Dosch <martin@mdosch.de>
2020-07-03 18:50:04 +0200

    mod_watch_spam_reports: Define variable prior to using it

--- mod_watch_spam_reports/mod_watch_spam_reports.lua
+++ mod_watch_spam_reports/mod_watch_spam_reports.lua
@@ -6,12 +6,12 @@ local host = module.host;
 module:depends("spam_reporting")
 
 module:hook("spam_reporting/spam-report", function(event)
-	local report = reporter_bare_jid.." reported "..event.jid.." as spammer: "..event.reason
 	local reporter_bare_jid = jid.bare(event.stanza.attr.from)
+	local report = reporter_bare_jid.." reported "..event.jid.." as spammer: "..event.reason
 	for admin_jid in admins
 		do
 			module:send(st.message({from=host,
 			type="chat",to=admin_jid},
-                	report));
+			report));
 		end
 end)
