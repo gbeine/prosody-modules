876f0119e0ef33f153a8a13cedcd4c8c163565d6
Matthew Wild <mwild1@gmail.com>
2020-09-11 13:53:00 +0100

    mod_http_libjs: Add compatibility with Prosody 0.11.x

--- mod_http_libjs/mod_http_libjs.lua
+++ mod_http_libjs/mod_http_libjs.lua
@@ -3,11 +3,19 @@ local mime_map = module:shared("/*/http_files/mime").types or {
 	js = "application/javascript",
 };
 
+local serve;
+if not pcall(function ()
+	local http_files = require "net.http.files";
+	serve = http_files.serve;
+end) then
+	serve = module:depends"http_files".serve;
+end
+
 local libjs_path = module:get_option_string("libjs_path", "/usr/share/javascript");
 
 module:provides("http", {
 		default_path = "/share";
 		route = {
-			["GET /*"] = require "net.http.files".serve({ path = libjs_path, mime_map = mime_map });
+			["GET /*"] = serve({ path = libjs_path, mime_map = mime_map });
 		}
 	});
